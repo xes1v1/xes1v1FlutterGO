4bb81c7dd44a2980d60e29e479709aa8a2d23143
王帅 <wangshuai14@100tal.com>
2019-11-06 19:40:23 +0800

    王帅Mixin作业

--- lib/homework/mixin/01homework_ws.dart
+++ lib/homework/mixin/01homework_ws.dart
@@ -1,3 +1,10 @@
+/*
+ * Created with Android Studio.
+ * User: wangshuai
+ * Date: 2019-11-06
+ * Time: 19:36
+ * tartget: TODO 添加本文件描述信息
+ */
 class Base {
   void say() {
     print("a");
@@ -13,18 +20,20 @@ mixin A on Base {
 mixin B on Base {
   void say() {
     print("b");
+    super.say();
   }
 }
 
 mixin C on Base {
   void say() {
     print("c");
+    super.say();
   }
 }
 
 // todo 改造你认为需要改造的类 满足 main 方法输出的要求
-class PrintSay {
-  void say() {}
+class PrintSay extends Base with A,B,C {
+
 }
 
 void main() {
