cae4fbb54236bef151dda30495593ef6a9c5fabe
小懒猴 <zhangjingxi@100tal.com>
2019-11-06 19:37:37 +0800

    张景希作业

---
+++ lib/homework/mixin/zhangjingxi.dart
@@ -0,0 +1,34 @@
+class Base {
+  void say() {
+    print("a");
+  }
+}
+
+mixin A on Base {
+  void say() {
+    super.say();
+    print("a");
+  }
+}
+
+mixin B on Base {
+  void say() {
+    super.say();
+    print("b");
+  }
+}
+
+mixin C on Base {
+  void say() {
+    print("c");
+  }
+}
+
+// todo 改造你认为需要改造的类 满足 main 方法输出的要求
+class PrintSay extends Base with C, B, A  {
+}
+
+void main() {
+  // todo 修改以上代码，输出 c b a 的顺序
+  PrintSay().say();
+}
