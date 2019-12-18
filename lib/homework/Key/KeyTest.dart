1ab9940ae74f225550cdb50a9257363742e03657
wanghuaqiang <wanghuaqiang@100tal.com>
2019-12-18 14:25:42 +0800

    key作业

--- lib/homework/Key/KeyTest.dart
+++ lib/homework/Key/KeyTest.dart
@@ -11,18 +11,77 @@ import 'package:flutter/material.dart';
 
 Widget keyTest() {
   return new MaterialApp(
-    home: new KeyTest(),
+    home: new KeyTest1(),
   );
 }
 
-class KeyTest extends StatefulWidget {
-  KeyTest({Key key}) : super(key: key);
+// 作业
+
+class KeyTest2 extends StatefulWidget {
+  KeyTest2({Key key}) : super(key: key);
 
   @override
-  _KeyState createState() => _KeyState();
+  _Key2State createState() => _Key2State();
 }
 
-class _KeyState extends State<KeyTest> {
+class _Key2State extends State<KeyTest2> {
+  bool _open = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(title: new Text('key test')),
+      body: Center(
+        child: SwitchWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _open = !_open;
+        },
+        child: Icon(Icons.swap_horizontal_circle),
+      ),
+    );
+  }
+}
+
+// 一个独立的switch小组件
+class SwitchWidget extends StatefulWidget {
+  SwitchWidget({Key key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => SwitchWidgetState();
+}
+
+class SwitchWidgetState extends State<SwitchWidget> {
+  bool _open = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Switch(
+          value: _open,
+        ),
+      ],
+    );
+  }
+
+  void changSwitch(bool open) {
+    setState(() {
+      _open = open;
+    });
+  }
+}
+
+// 例子
+class KeyTest1 extends StatefulWidget {
+  KeyTest1({Key key}) : super(key: key);
+
+  @override
+  _Key1State createState() => _Key1State();
+}
+
+class _Key1State extends State<KeyTest1> {
   double value = 0.0;
   final configKey = GlobalKey<ConfigWidgetState>();
 
@@ -45,7 +104,7 @@ class _KeyState extends State<KeyTest> {
   }
 }
 
-// 一个独立的widget小组件
+// 一个独立的滑动小组件
 class ConfigWidget extends StatefulWidget {
   ConfigWidget({Key key}) : super(key: key);
 
@@ -55,6 +114,7 @@ class ConfigWidget extends StatefulWidget {
 
 class ConfigWidgetState extends State<ConfigWidget> {
   double _sliderValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
